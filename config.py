a42d359d229fd1a75aae1d116adcefe1faa02bf7
Xerow42 <98987122+Xerow42@users.noreply.github.com>
2025-08-27 14:33:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 EMAIL_SENDER = "klamrabeta19@gmail.com"
 EMAIL_PASSWORD = "SCRIPTED"
-SECRET_KEY = "supersecretkey"
+SECRET_KEY = "SCRIPTED"
 DATABASE = "crm.db"
